3504e52c2c96e8451efe2b3cb5d37610aa7c4ae5
Mitch Blaser <mitchell.1080media@gmail.com>
2021-04-27 02:46:53 +1000

    Added Pin Options/Timeout

--- gpio_shutdown.py
+++ gpio_shutdown.py
@@ -1,11 +1,15 @@
 import os
+import time
 import RPi.GPIO as GPIO
+
+pin = 40
 
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 while True:
-    if GPIO.input(2) == GPIO.HIGH:
+    time.sleep(0.05)
+    if GPIO.input(pin) == GPIO.HIGH:
         print("Shutdown on GPIO.")
         os.system("sudo shutdown now")
