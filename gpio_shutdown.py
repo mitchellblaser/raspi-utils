05a9efad16739f4ba4fcbd1de9ba714ba34ce543
Mitch Blaser <mitchell.1080media@gmail.com>
2021-04-27 03:40:33 +1000

    Added 1s button hold check

--- gpio_shutdown.py
+++ gpio_shutdown.py
@@ -8,8 +8,14 @@ GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
+last_input = 0
+
 while True:
-    time.sleep(0.05)
+    time.sleep(0.5)
     if GPIO.input(pin) == GPIO.HIGH:
-        print("Shutdown on GPIO.")
-        os.system("sudo shutdown now")
+        if last_input == 1:
+            print("Shutdown on GPIO.")
+            os.system("sudo shutdown now")
+        last_input = 1
+    else:
+        last_input = 0
